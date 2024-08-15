80c7ea9ef08d2493b3bdc6c20a65e7846bc6eae5
pacexy <pacexy@gmail.com>
2024-08-15 19:05:12 +0800

    draft config

---
+++ src/fansubs/kitauji.ts
@@ -0,0 +1,12 @@
+export const config = {
+  name: 'kitauji',
+  title: '北宇治',
+  logo: 'https://avatars.githubusercontent.com/u/121954830?s=200&v=4',
+  repo: 'https://github.com/Kitauji-Sub/Subtitles',
+  website: 'https://kitauji.inari.site',
+  social: {
+    telegram: 'https://t.me/KitaUji',
+    qq: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=ED0Qrm-TRS5mtfTQwrFMBQEtsrVyqAQg&authKey=fov%2FXdbhFFvjnKwZX3u7xGkY7LwlfIaplbcLu64Zbcrv2hxHAxuj2aqjDhSlQba7&noverify=0&group_code=232487445',
+    bilibili: 'https://space.bilibili.com/3546697424702177',
+  },
+}
