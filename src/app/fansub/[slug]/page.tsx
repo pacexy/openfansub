db30ab8b362d002205a48e4289a17719eb7ce932
pacexy <pacexy@gmail.com>
2024-08-18 07:05:54 +0800

    feat: hide avatar if not configured

--- src/app/fansub/[slug]/page.tsx
+++ src/app/fansub/[slug]/page.tsx
@@ -66,12 +66,11 @@ export default function FansubPage({ params }: { params: { slug: string } }) {
       <div className="mt-4 grid grid-cols-1 gap-16 md:grid-cols-3">
         {/* left */}
         <div className="md:col-span-1">
-          <div className="mb-4 aspect-square w-full rounded-full bg-muted">
-            {config.avatar && (
-              // eslint-disable-next-line @next/next/no-img-element
+          {config.avatar && (
+            <div className="mb-4 aspect-square w-full rounded-full">
               <img src={config.avatar} alt={config.name} className="w-full" />
-            )}
-          </div>
+            </div>
+          )}
           <h1 className="mb-2 text-2xl font-bold">{config.name}</h1>
           <p className="mb-4 text-muted-foreground">{config.description}</p>
           <ul className="space-y-3">
