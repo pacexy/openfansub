572a02bc74bfbdcac6a117b5ccf4ec15cdabb3de
pacexy <pacexy@gmail.com>
2024-08-17 16:46:14 +0800

    add sweetsub

---
+++ src/fansubs/sweetsub.ts
@@ -0,0 +1,18 @@
+import type { FansubConfig } from '~/lib/fansub'
+
+const config = {
+  slug: 'sweetsub',
+  name: 'SweetSub',
+  repo: {
+    owner: 'SweetSub',
+    name: 'SweetSub',
+    branch: 'master',
+  },
+  links: {
+    website:
+      'https://arontaus-my.sharepoint.com/:x:/g/personal/pu_theparagonschool_org/ERp4IaR7jo9ItKMvmaeK6ewBPT-7LTEzBBQf8_nSUzw3Fg?e=WiJhnQ',
+    telegram: 'https://t.me/SweetSub',
+  },
+} satisfies FansubConfig
+
+export default config
