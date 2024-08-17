3319af43664d30bb7f75df3d1cc9640dea51d2cf
pacexy <pacexy@gmail.com>
2024-08-18 07:06:57 +0800

    init config

---
+++ src/fansubs/zhuazhua.ts
@@ -0,0 +1,18 @@
+import type { FansubConfig } from '~/lib/fansub'
+
+const config = {
+  slug: 'zhuazhua',
+  name: '爪爪字幕组',
+  description: '我们是爪爪字幕组，是由一群喜欢动画的人们组成的非盈利字幕组。',
+  avatar: 'https://avatars.githubusercontent.com/u/114382580?v=4',
+  repo: {
+    owner: 'ZhuaZhuaSub',
+    name: 'ZhuaZhuaStudio',
+    branch: 'main',
+  },
+  links: {
+    website: 'https://kitauji.inari.site',
+  },
+} satisfies FansubConfig
+
+export default config
