da79a2949fc420edcc7f754012ac0fac93a6034a
pacexy <pacexy@gmail.com>
2024-09-17 14:48:55 +0800

    dynamically render search results

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -15,30 +15,32 @@ export function Search({ fansubConfigs }: { fansubConfigs: FansubConfig[] }) {
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
       />
-      <ul className="absolute right-0 mt-4 h-80 w-96 space-y-1 overflow-auto border bg-card px-2 py-1">
-        {fansubConfigs.map((config) =>
-          Object.values(config.subtitleDirs ?? {}).map((dirs) =>
-            dirs
-              .filter((dir) => keyword.length > 1 && reg.test(dir.path))
-              .map((dir) => (
-                <li key={dir.path}>
-                  <span className="mr-2 bg-muted px-1 py-0.5 text-sm text-muted-foreground">
-                    {config.name}
-                  </span>
-                  <a
-                    className="text-foreground"
-                    dangerouslySetInnerHTML={{
-                      __html: dir.path.replace(
-                        reg,
-                        (match) => `<mark>${match}</mark>`,
-                      ),
-                    }}
-                  ></a>
-                </li>
-              )),
-          ),
-        )}
-      </ul>
+      {keyword.length > 1 && (
+        <ul className="absolute right-0 mt-4 h-80 w-96 space-y-1 overflow-auto border bg-card px-2 py-1">
+          {fansubConfigs.map((config) =>
+            Object.values(config.subtitleDirs ?? {}).map((dirs) =>
+              dirs
+                .filter((dir) => reg.test(dir.path))
+                .map((dir) => (
+                  <li key={dir.path}>
+                    <span className="mr-2 bg-muted px-1 py-0.5 text-sm text-muted-foreground">
+                      {config.name}
+                    </span>
+                    <a
+                      className="text-foreground"
+                      dangerouslySetInnerHTML={{
+                        __html: dir.path.replace(
+                          reg,
+                          (match) => `<mark>${match}</mark>`,
+                        ),
+                      }}
+                    ></a>
+                  </li>
+                )),
+            ),
+          )}
+        </ul>
+      )}
     </div>
   )
 }
