c12bd94b378f2e679889d980f7b6f684401fc784
pacexy <pacexy@gmail.com>
2024-10-04 16:24:18 +0800

    feat: `fansub.status` indicator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import { fansubSlugs, importFansub } from '~/lib/fansub'
+import { cn } from '~/lib/utils'
 import Link from 'next/link'
 
 export default async function Home() {
+  const fansubs = await Promise.all(fansubSlugs.map(importFansub))
   return (
     <div>
       <nav>
-        <ul>
-          {fansubSlugs.map(async (slug) => (
-            <li key={slug}>
-              <Link href={`/fansub/${slug}`}>
-                {(await importFansub(slug)).name}
-              </Link>
+        <ul className="space-y-3">
+          {fansubs.map((f) => (
+            <li
+              key={f.slug}
+              className={cn(
+                'border-l-4 pl-2',
+                f.status !== 'inactive' && 'border-green-200',
+              )}
+            >
+              <Link href={`/fansub/${f.slug}`}>{f.name}</Link>
             </li>
           ))}
         </ul>
